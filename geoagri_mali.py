2a1078898d0e5884e8b51ba585b1b723e4260a1b
Moccamara <mahamadoucamaramoc@outlook.com>
2026-04-27 17:35:28 +0000

    Update geoagri_mali.py

--- geoagri_mali.py
+++ geoagri_mali.py
@@ -338,7 +338,7 @@ elif search_result is None and region == "No filter":
 
     m = folium.Map(
         location=[17.5, -4],
-        zoom_start=8,
+        zoom_start=6,
         tiles=None
     )
 
